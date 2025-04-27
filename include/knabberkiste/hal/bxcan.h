1067d7d90180449561f89bd7db709feef833e648
Gabriel Heinzer <dev@gabrielheinzer.ch>
2025-04-27 21:20:36 +0200

    Added bxCAN HAL

--- include/knabberkiste/hal/bxcan.h
+++ include/knabberkiste/hal/bxcan.h
@@ -2,9 +2,44 @@
  * @file bxcan.h
  * @author Gabriel Heinzer
  * @brief HAL for bxCAN (basic and extendend controller area network) peripheral.
+ * 
+ * @section bxcan_initialization Initialization
+ * Before accessing any bxCAN functionality, you must first initialize the peripheral
+ * using @ref can_init(). This function takes the bitrate as an argument, which must
+ * match the bitrate of the other CAN nodes. You can also configure a test mode if
+ * required, but in most cases, @ref CAN_TESTMODE_NONE will probably be best.
+ * 
+ * After initializing the peripheral itself, you should also configure some identifier
+ * filtering. Only messages which pass through at least one filter are acknowledged, which
+ * means that messages which aren't acknowledged by at least one CAN node cause an error
+ * on the sending node.
+ * 
+ * For the values you need to pass to @ref can_configure_filter_bank, please refer to the
+ * reference manual RM0316 from STMicroelectronics.
+ * 
+ * @section bxcan_frame_transmission Frame transmission
+ * Use @ref can_transmit_frame() to schedule a frame for tranmission. Messages are buffered
+ * internally in a queue when they can't be transmitted immediately. Then, they are
+ * immediately (and automatically) transmitted when the next transmit mailbox becomes
+ * available.
+ * 
+ * @section bxcan_frame_reception Frame reception
+ * Received frames are automatically converted in a @ref CAN_Frame_t structure and then passed
+ * to @ref can_recv_callback(). This function should be defined by your application. It is called
+ * for every frame in an interrupt context. Don't perform any lengthy processing in this callback,
+ * as this blocks the whole microcontroller (because it's executed inside the ISR).
+ * 
+ * @section bxcan_error_handling Error handling
+ * When an error occurs during transmission, @ref can_error_callback() is called. Note that this
+ * may not be called for every frame which fails to get transmitted, but only once if an error
+ * condition stays active.
  */
 
 #pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <knabberkiste/io.h>
 
 /**
  * @brief Structure representing a single CAN frame.
@@ -23,13 +58,179 @@ typedef struct {
 } CAN_Frame_t;
 
 /**
- * @brief Initializes the CAN peripheral.
+ * @brief Enumerator for the available CAN test mode flags. You may
+ * OR these to enable multiple test modes simulateously.
  */
-void can_init();
+typedef enum {
+    /// @brief Disables all test modes
+    CAN_TESTMODE_NONE = 0,
+    /// @brief Enables loopback mode.  Can be used in conjunction with @ref CAN_TESTMODE_SILENT.
+    CAN_TESTMODE_LOOPBACK = CAN_BTR_LBKM,
+    /// @brief Enables silent mode. Can be used in conjunction with @ref CAN_TESTMODE_LOOPBACK.
+    CAN_TESTMODE_SILENT = CAN_BTR_SILM
+} CAN_TestMode_t;
 
 /**
- * @brief Sends a single CAN frame.
+ * @brief Enumerator for bxCAN filter numbers.
+ */
+typedef enum {
+    /// @brief bxCAN filter bank 0
+    CAN_FILTERBANK_0 = 0,
+    /// @brief bxCAN filter bank 1
+    CAN_FILTERBANK_1,
+    /// @brief bxCAN filter bank 2
+    CAN_FILTERBANK_2,
+    /// @brief bxCAN filter bank 3
+    CAN_FILTERBANK_3,
+    /// @brief bxCAN filter bank 4
+    CAN_FILTERBANK_4,
+    /// @brief bxCAN filter bank 5
+    CAN_FILTERBANK_5,
+    /// @brief bxCAN filter bank 6
+    CAN_FILTERBANK_6,
+    /// @brief bxCAN filter bank 8
+    CAN_FILTERBANK_8,
+    /// @brief bxCAN filter bank 9
+    CAN_FILTERBANK_9,
+    /// @brief bxCAN filter bank 10
+    CAN_FILTERBANK_10,
+    /// @brief bxCAN filter bank 11
+    CAN_FILTERBANK_11,
+    /// @brief bxCAN filter bank 12
+    CAN_FILTERBANK_12,
+    /// @brief bxCAN filter bank 13
+    CAN_FILTERBANK_13
+} CAN_FilterBank_t;
+
+/**
+ * @brief Enumerator for bxCAN filter bank width.
+ */
+typedef enum {
+    /// @brief Two 16-bit filters for the STDID[10:0], RTR, IDE, and EXTID[17:15] bits
+    CAN_FILTERBANK_WIDTH_16BIT = 0,
+    /// @brief One 32-bit filter for the STDID[10:0], EXTID[17:0], IDE, and RTR bits
+    CAN_FILTERBANK_WIDTH_32BIT = 1
+} CAN_FilterBankWidth_t;
+
+/**
+ * @brief Enumerator for bxCAN filter bank width.
+ */
+typedef enum {
+    /**
+     * @brief In mask mode the identifier registers are associated with mask registers specifying which
+     * bits of the identifier are handled as “must match” or as “don’t care”.
+     */
+    CAN_FILTERBANK_MODE_MASK = 0,
+    /**
+     * @brief In identifier list mode, the mask registers are used as identifier registers. Thus, instead of
+     * defining an identifier and a mask, two identifiers are specified, doubling the number of single
+     * identifiers. All bits of the incoming identifier must match the bits specified in the filter
+     * registers.
+     */
+    CAN_FILTERBANK_MODE_LIST = 1
+} CAN_FilterBankMode_t;
+
+/**
+ * @brief Enumerator for bxCAN FIFO numbers.
+ */
+typedef enum{
+    CAN_FIFO_0 = 0,
+    CAN_FIFO_1 = 1
+} CAN_FIFO_t;
+
+/**
+ * @brief Structure representing a single received CAN frame. Note that
+ * this has some additional data members compared to CAN_Frame_t.
+ */
+typedef struct {
+    /// @brief CAN frame which was received.
+    CAN_Frame_t frame;
+    /// @brief FIFO in which the CAN frame was stored.
+    CAN_FIFO_t fifo;
+    /// @brief Identifier filter which matched the CAN frame.
+    CAN_FilterBank_t filter_match;
+} CAN_ReceivedFrame_t;
+
+/**
+ * @brief Enumerator for all possible CAN errors codes.
+ */
+typedef enum {
+    /// @brief No error has been detected.
+    CAN_ERR_NONE = 0,
+    /// @brief Bit stuffing error.
+    CAN_ERR_STUFF_ERROR = 0b001,
+    /// @brief Frame form error.
+    CAN_ERR_FORM_ERROR = 0b010,
+    /// @brief Frame was not acknowledged.
+    CAN_ERR_ACKNOWLEDGEMENT_ERROR = 0b011,
+    /// @brief Recessive bit could not be sent.
+    CAN_ERR_BIT_RECESSIVE_ERROR = 0b100,
+    /// @brief Dominant bit could not be sent.
+    CAN_ERR_BIT_DOMINANT_ERROR = 0b101,
+    /// @brief CRC invalid.
+    CAN_ERR_CRC_ERROR = 0b110,
+    /// @brief Software-set error.
+    CAN_ERR_SOFTWARE_ERROR = 0b111,
+    /// @brief FIFO0 has been overrun. Messages have been lost.
+    CAN_ERR_FIFO0_OVERRUN,
+    /// @brief FIFO1 has been overrun. Messages have been lost.
+    CAN_ERR_FIFO1_OVERRUN
+} CAN_ErrorCode_t;
+
+/**
+ * @brief Initializes the CAN peripheral.
+ * 
+ * @warning You must first initalize the CAN TX and CAN RX pins with their alternate
+ * function mapping, otherwise this initialization procedure will never return.
+ * 
+ * @param bitrate Bitrate, in bits per second, to initialize the peripheral with.
+ * @param testMode Test mode flags to enable.
+ */
+void can_init(
+    uint32_t bitrate,
+    CAN_TestMode_t testMode
+);
+
+/**
+ * @brief Configures the specified filter bank.
+ * 
+ * @param filter The filter bank to configure.
+ * @param fifo FIFO to which the filter bank will be assigned to.
+ * @param width Width of the filter bank. Refer to the reference manual for more information.
+ * @param mode Mode of the filter bank. Refer to the reference manual for more information.
+ * @param FxR1_val Value of the first filter register. Refer to the reference manual for more information.
+ * @param FxR2_val Value of the first filter register. Refer to the reference manual for more information.
+ */
+void can_configure_filter_bank(
+    CAN_FilterBank_t filter,
+    CAN_FIFO_t fifo,
+    CAN_FilterBankWidth_t width,
+    CAN_FilterBankMode_t mode,
+    uint32_t FxR1_val,
+    uint32_t FxR2_val
+);
+
+/**
+ * @brief Transmit a single CAN frame. The CAN frame may be buffered internally
+ * if the no transmit mailbox is available.
  * 
  * @param frame The frame to send.
  */
-void can_send_frame(CAN_Frame_t *frame);
+void can_transmit_frame(CAN_Frame_t* frame);
+
+/**
+ * @brief Callback function called when a frame has been received. This function is 
+ * bound weakly internally, i.e. can, or should, be implemented by the application.
+ * 
+ * @param frame Frame which was received.
+ */
+void can_recv_callback(CAN_ReceivedFrame_t frame) __attribute__((weak));
+
+/**
+ * @brief Callback function called when an error occurred in the CAN peripheral.
+ * This function is bound weakly internally, i.e. can, or should, be implemented
+ * by the application.
+ * 
+ * @param error_code Error code which caused the interrupt.
+ */
+void can_error_callback(CAN_ErrorCode_t error_code) __attribute__((weak));
---
+++ src/knabberkiste/hal/bxcan.c
@@ -0,0 +1,259 @@
+#include <knabberkiste/hal/bxcan.h>
+#include <knabberkiste/hal/vcp_debug.h>
+#include <knabberkiste/hal/gpio.h>
+#include <knabberkiste/util/bit_manipulation.h>
+#include <knabberkiste/util/fifo.h>
+#include <knabberkiste/util/error.h>
+#include <knabberkiste/io.h>
+#include <string.h>
+
+#define BXCAN_TX_QUEUE_SIZE 4
+
+fifo_declare_qualifier(CAN_Frame_t, bxcan_tx_queue, BXCAN_TX_QUEUE_SIZE, static);
+
+// Internal functions
+CAN_ReceivedFrame_t can_read_frame_from_fifo(CAN_FIFO_t fifo);
+
+// Interrupt handlers
+void USB_HP_CAN_TX_IRQHandler() {
+    // Transmit interrupt, this is fired by the following bits being set:
+    //  - TSR -> RQCP0 (Request completed mailbox 0)
+    //  - TSR -> RQCP1 (Request completed mailbox 1)
+    //  - TSR -> RQCP2 (Request completed mailbox 2)
+    // Of course, this is dependent on the enabled interrupts
+
+    // Clear the RQCPx and TXOKx bits
+    CLEAR_MASK(CAN->TSR, CAN_TSR_RQCP0 | CAN_TSR_RQCP1 | CAN_TSR_RQCP2);
+    CLEAR_MASK(CAN->TSR, CAN_TSR_TXOK0 | CAN_TSR_TXOK1 | CAN_TSR_TXOK2);
+
+    // Transmit the next message in the transmit queue
+    if(!fifo_empty(bxcan_tx_queue)) {
+        CAN_Frame_t nextFrame;
+        fifo_get(bxcan_tx_queue, nextFrame);
+        can_transmit_frame(&nextFrame);
+    }
+}
+
+void USB_LP_CAN_RX0_IRQHandler() {
+    // FIFO 0 receive interrupt, this is fired by the following bits being set:
+    //  - RF0R -> FMP0  (FIFO 0 message pending)
+    //  - RF0R -> FULL0 (FIFO 0 full)
+    //  - RF0R -> FOVR0 (FIFO 0 overrun)
+    // Of course, this is dependent on the enabled interrupts. See can_init
+    // for more information.
+
+    if(READ_MASK(CAN->RF0R, CAN_RF0R_FMP0)) {
+        CAN_ReceivedFrame_t frame = can_read_frame_from_fifo(CAN_FIFO_0);
+
+        can_recv_callback(frame);
+
+        // Clear the FMP1 bit
+        CLEAR_MASK(CAN->RF0R, CAN_RF0R_FMP0);
+    } else if(READ_MASK(CAN->RF0R, CAN_RF0R_FOVR0)) {
+        can_error_callback(CAN_ERR_FIFO0_OVERRUN);
+    }
+}
+
+void CAN_RX1_IRQHandler() {
+    // FIFO 1 receive interrupt, this is fired by the following bits being set:
+    //  - RF1R -> FMP1  (FIFO 1 message pending)
+    //  - RF1R -> FULL1 (FIFO 1 full)
+    //  - RF1R -> FOVR1 (FIFO 1 overrun)
+    // Of course, this is dependent on the enabled interrupts. See can_init
+    // for more information.
+    
+    if(READ_MASK(CAN->RF1R, CAN_RF1R_FMP1)) {
+        CAN_ReceivedFrame_t frame = can_read_frame_from_fifo(CAN_FIFO_1);
+
+        can_recv_callback(frame);
+
+        // Clear the FMP1 bit
+        CLEAR_MASK(CAN->RF1R, CAN_RF1R_FMP1);
+    } else if(READ_MASK(CAN->RF1R, CAN_RF1R_FOVR1)) {
+        can_error_callback(CAN_ERR_FIFO1_OVERRUN);
+    }
+}
+
+void CAN_SCE_IRQHandler() {
+    // Status change/error interrupt, this is fired by the following bits being set:
+    //  - ESR -> EWGF (Error warning flag interrupt, receive error counter >= 96 or transmit error counter >= 96)
+    //  - ESR -> EPVF (Error passive flag interrupt, passive limit has been reached: receive/transmit error counter > 127)
+    //  - ESR -> BOFF (Buss-off flag interrupt, transmit error counter > 255)
+    //  - ESR -> LEC (Some kind of error has been detected)
+    //  - MSR -> WKUI (Wake-up interrupt)
+    //  - MSR -> SLAKI (Sleep acknowledge interrupt)
+    // Of course, this is dependent on the enabled interrupts
+    CAN_ErrorCode_t error_code = READ_MASK_OFFSET(CAN->ESR, 0b111, CAN_ESR_LEC_Pos);
+
+    // SLAKI and WKUI are ignored
+    if(error_code) {
+        can_error_callback(error_code);
+    }
+}
+
+void can_init(uint32_t bitrate, CAN_TestMode_t testMode) {
+    // Enable the clock for the bxCAN peripheral
+    SET_MASK(RCC->APB1ENR, RCC_APB1ENR_CANEN);
+
+    // Reset bxCAN peripheral
+    SET_MASK(RCC->APB1RSTR, RCC_APB1RSTR_CANRST);
+    CLEAR_MASK(RCC->APB1RSTR, RCC_APB1RSTR_CANRST);
+
+    // Disable bxCAN sleep mode
+    CLEAR_MASK(CAN->MCR, CAN_MCR_SLEEP);
+
+    // Enable bxCAN initialization mode
+    SET_MASK(CAN->MCR, CAN_MCR_INRQ); // Trigger initialization request
+    while(!READ_MASK(CAN->MSR, CAN_MSR_INAK)); // Wait for initialization acknowledgement
+
+    // Enable automatic bus-off recovery
+    SET_MASK(CAN->MCR, CAN_MCR_ABOM);
+
+    // Disable CAN debug freeze
+    SET_BIT(CAN->MCR, 16);
+
+    // Set transmit priority be request order
+    SET_MASK(CAN->MCR, CAN_MCR_TXFP);
+
+    // Enable receive FIFO locked mode
+    SET_MASK(CAN->MCR, CAN_MCR_RFLM);
+
+    // Enable interrutps
+    SET_MASK(CAN->IER, CAN_IER_ERRIE); // Error interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_LECIE); // Last error code interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_BOFIE); // Bus-off interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_EPVIE); // Error passive interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_EWGIE); // Error warning interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_FOVIE1); // FIFO 1 overrun interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_FMPIE1); // FIFO 1 message pending interrupt enable
+    CLEAR_MASK(CAN->IER, CAN_IER_FFIE1); // FIFO 1 full interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_FOVIE0); // FIFO 0 overrun interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_FMPIE0); // FIFO 0 message pending interrupt enable
+    CLEAR_MASK(CAN->IER, CAN_IER_FFIE0); // FIFO 0 full interrupt enable
+    SET_MASK(CAN->IER, CAN_IER_TMEIE); // Transmit mailbox empty interrupt enable
+
+    // Enable interrupts
+    NVIC_EnableIRQ(CAN_TX_IRQn);
+    NVIC_EnableIRQ(CAN_RX0_IRQn);
+    NVIC_EnableIRQ(CAN_RX1_IRQn);
+    NVIC_EnableIRQ(CAN_SCE_IRQn);
+
+    // Configure test mode
+    SET_MASK(CAN->BTR, testMode);
+
+    // Configure bitrate
+    uint8_t ts1_val = READ_MASK_OFFSET(CAN->BTR, 0b1111, CAN_BTR_TS1_Pos);
+    uint8_t ts2_val = READ_MASK_OFFSET(CAN->BTR, 0b111, CAN_BTR_TS2_Pos);
+    uint32_t time_quantum_frequency = bitrate * (3 + ts1_val + ts2_val);
+    uint32_t brp_val = (SystemCoreClock / time_quantum_frequency) - 1;
+    WRITE_MASK_OFFSET(CAN->BTR, 0b111111111, brp_val, CAN_BTR_BRP_Pos);
+
+    // Enable bxCAN
+    CLEAR_MASK(CAN->MCR, CAN_MCR_INRQ);
+    while(READ_MASK(CAN->MSR, CAN_MSR_INAK)); // Wait for initialization acknowledgement
+
+    // Enable IRQs, needed for CAN
+    __enable_irq();
+}
+
+void can_configure_filter_bank(
+    CAN_FilterBank_t filter,
+    CAN_FIFO_t fifo,
+    CAN_FilterBankWidth_t width,
+    CAN_FilterBankMode_t mode,
+    uint32_t FxR1_val,
+    uint32_t FxR2_val
+) {
+    // Enter filter initialization mode
+    SET_MASK(CAN->FMR, CAN_FMR_FINIT);
+
+    // Set the filter's mode
+    WRITE_BIT(CAN->FM1R, filter, mode);
+
+    // Set the filter's width
+    WRITE_BIT(CAN->FS1R, filter, width);
+
+    // Assign the filter to it's FIFO
+    WRITE_BIT(CAN->FFA1R, filter, fifo);
+
+    // Activate the filter
+    SET_BIT(CAN->FA1R, filter);
+
+    // Write the filter value
+    CAN->sFilterRegister[filter].FR1 = FxR1_val;
+    CAN->sFilterRegister[filter].FR2 = FxR2_val;
+
+    // Leave filter initialization mode
+    CLEAR_MASK(CAN->FMR, CAN_FMR_FINIT); 
+}
+
+void can_transmit_frame(CAN_Frame_t* frame) {
+    // Check if a mailbox is free
+    bool mailbox_free = READ_MASK(CAN->TSR, CAN_TSR_TME);
+
+    if(mailbox_free) {
+        __disable_irq();
+        uint8_t mailbox_id = READ_MASK_OFFSET(CAN->TSR, 0b11, CAN_TSR_CODE_Pos);
+        CAN_TxMailBox_TypeDef* mailbox = &(CAN->sTxMailBox[mailbox_id]);
+
+        // Set the identifier register
+        WRITE_BIT(mailbox->TIR, CAN_TI0R_RTR_Pos, frame->rtr);
+        WRITE_BIT(mailbox->TIR, CAN_TI0R_IDE_Pos, frame->id_extended);
+
+        if(frame->id_extended) {
+            // Configure the extended ID
+            WRITE_MASK_OFFSET(mailbox->TIR, 0x3FFFF, frame->id, CAN_TI0R_EXID_Pos);
+        } else {
+            // Configure the standard ID
+            WRITE_MASK_OFFSET(mailbox->TIR, 0x7FF, frame->id, CAN_TI0R_STID_Pos);
+        }
+
+        // Set the data length control register
+        WRITE_MASK_OFFSET(mailbox->TDTR, 0xF, frame->dlc, CAN_TDT0R_DLC_Pos);
+
+        // Set the data low register
+        mailbox->TDLR = ((uint32_t*)(frame->data))[0];
+        mailbox->TDHR = ((uint32_t*)(frame->data))[1];
+
+        // Set the transmit request bit
+        SET_MASK(mailbox->TIR, CAN_TI0R_TXRQ);
+        __enable_irq();
+    } else {
+        // Append the frame to the transmit queue
+        fifo_put(bxcan_tx_queue, *frame);
+    }
+}
+
+CAN_ReceivedFrame_t can_read_frame_from_fifo(CAN_FIFO_t fifo) {
+    CAN_FIFOMailBox_TypeDef* mailbox = &(CAN->sFIFOMailBox[fifo]);
+
+    // Read the CAN frame
+    CAN_ReceivedFrame_t recv_frame;
+
+    // Read the receiving information
+    recv_frame.fifo = fifo;
+    recv_frame.filter_match = READ_MASK_OFFSET(mailbox->RDTR, 0xFF, CAN_RDT0R_FMI_Pos);
+
+    // Read the CAN frame itself
+    recv_frame.frame.dlc = READ_MASK_OFFSET(mailbox->RDTR, 0b1111, CAN_RDT0R_DLC_Pos);
+
+    recv_frame.frame.rtr = READ_MASK_OFFSET(mailbox->RIR, 0b1, CAN_RI0R_RTR_Pos);
+    recv_frame.frame.id_extended = READ_MASK_OFFSET(mailbox->RIR, 0b1, CAN_RI0R_IDE_Pos);
+    if(recv_frame.frame.id_extended) {
+        recv_frame.frame.id = READ_MASK_OFFSET(mailbox->RIR, 0x3FFFF, CAN_RI0R_EXID_Pos);
+    } else {
+        recv_frame.frame.id = READ_MASK_OFFSET(mailbox->RIR, 0x7FF, CAN_RI0R_STID_Pos);
+    }
+
+    ((uint32_t*)(recv_frame.frame.data))[0] = mailbox->RDLR;
+    ((uint32_t*)(recv_frame.frame.data))[1] = mailbox->RDHR;
+
+    // Release the mailbox
+    if(fifo == CAN_FIFO_0) {
+        SET_MASK(CAN->RF0R, CAN_RF0R_RFOM0);
+    } else {
+        SET_MASK(CAN->RF1R, CAN_RF1R_RFOM1);
+    }
+
+    return recv_frame;
+}
